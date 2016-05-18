771ad88da3d7d5f4ba568e0f30de34711c1a07b2
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-05-18 20:00:20 +0900

    wip

---
+++ src/fino/app.ts
@@ -0,0 +1,8 @@
+/// <reference path="./typings/main.d.ts" />
+
+import express = require('express');
+import path = require('path');
+import favicon = require('serve-favicon');
+import logger = require('morgan');
+import cookieParser = require('cookie-parser');
+import bodyParser = require('body-parser')
