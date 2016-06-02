bcdabf28053bae9e99506f85ca9f6f384ad4f362
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-06-02 15:15:46 +0900

    Update.

---
+++ _gulpfile.ts
@@ -0,0 +1,6 @@
+/// <reference path="./typings/main.d.ts" />
+
+import * as gulp from 'gulp';
+import * as rd from 'require-dir';
+
+rd('./gulp', {recurse: true});
--- gulpfile.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./typings/main.d.ts" />
-
-import * as gulp from 'gulp';
-import * as rd from 'require-dir';
-
-rd('./gulp', {recurse: true});
